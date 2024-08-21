87ebb80f4dc952a9d981ff970153f6021bd8b093
LupoArticoblu <wolf94-@libero.it>
2024-08-21 11:13:29 +0200

    commento errore empty

--- server.php
+++ server.php
@@ -3,6 +3,7 @@ $string = file_get_contents('dischi.json');
 $disc_list = json_decode($string, true);
 $result = [];
 
+//usare empty qui è un errore poiche l'oggetto indicizzato a 0 mi restituisce null e quindi mi fornirà come result la lista array di dischi
 if (!isset($_GET['discIndex'])) {
   //non invio la richiesta dettaglio e quindi stampo tutti i dischi
   $result = $disc_list;
